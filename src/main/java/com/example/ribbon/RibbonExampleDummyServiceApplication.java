82bfe970825a54d7daff006e4c100acdfd4c6aad
Joydeep Sur <joydeepsur6@gmail.com>
2020-04-26 21:28:26 +0530

    initial commit

---
+++ src/main/java/com/example/ribbon/RibbonExampleDummyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonExampleDummyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonExampleDummyServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/ribbon/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.ribbon.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ribbon/dummy/service")
+public class HelloController {
+
+	@Value("${server.port}")
+	private String port;
+
+	@GetMapping
+	public String getHello() {
+		return "Hello everyone!! from Port :" + port;
+	}
+}
